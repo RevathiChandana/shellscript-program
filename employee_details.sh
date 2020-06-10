c585664f804a15b17b7e58322da240daa64c57cb
Revathi Chandana <chandanarevathi30@gmail.com>
2020-06-10 14:23:58 +0530

    uc-2 : calculate daily employee wage by assuming that wage per hour is 20 and full day hour is 8

--- employee_details.sh
+++ employee_details.sh
@@ -1,16 +1,14 @@
 #! /bin/bash
 echo "welcome to employee wage computation"
 isPresent=$(( RANDOM%2 ))
-
+wage_per_hr=20
+full_day_hours=8
 if [ $isPresent == 0 ]
 then
         echo "Employee is Present"
+	Daily_emp_wage=$(($wage_per_hr*$full_day_hours))
+	echo "Daily employee wage : " $Daily_emp_wage
 else
         echo "Employee is not Present"
 fi
-wage_per_hr=20
-full_day_hours=8
-Daily_emp_wage=$(($wage_per_hr*$full_day_hours))
-echo "Daily employee wage : " $Daily_emp_wage
-part_time_hours=4
-echo "part time employee wage : " $(($wage_per_hr*$part_time_hours))
+
