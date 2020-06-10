20827d25fdd1eefb135dd0bc6c755b457642bfd0
Revathi Chandana <chandanarevathi30@gmail.com>
2020-06-10 14:55:20 +0530

    uc-5 : calculating wages for a month by assuming 20 working days per month

--- employee_details.sh
+++ employee_details.sh
@@ -1,13 +1,19 @@
-#! /bin/bash
+#! /bin/bash 
 isPartTime=1;
 isFullTime=2;
-empRate_PerHr=20;
-emp_check=$((RANDOM%3))
-case $emp_check in
-        $isFullTime)    emp_per_hr=8;;
-        $isPartTime)    emp_per_hr=4;;
-        *)      emp_per_hr=0;;
-esac
-salary=$(($empRate_PerHr*$emp_per_hr))
-echo " salary : " $salary
+total_salary=0;
+emp_rate_per_hr=20;
+Num_working_days=20;
+for (( day=1; day<=$Num_working_days; day++ ))
+do
+        emp_check=$((RANDOM%3));
+        case $emp_check in
+                $isPartTime)    emp_hr=4;;
+                $isFullTime)    emp_hr=8;;
+                *)      emp_hr=0;;
+        esac
+        salary=$(($emp_rate_per_hr*$emp_hr))
+        total_salary=$(($total_salary+$salary))
+done
+echo "total salary : " $total_salary
 
